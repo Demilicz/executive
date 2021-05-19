26f82b7b4196fe643620e1fb7c1daf936d33abbb
Demilicz <Necrolick92@gmail.com>
2021-05-19 21:30:20 +0200

    plus 2 easy sections

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -5,12 +5,31 @@ $(function() {
 
   $('.featured__slider').slick({
     draggable: false,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: true,
     responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
       {
         breakpoint: 992,
         settings: {
-          arrows: false,
-          draggable: true
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 578,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          draggable: true,
         }
       }
     ]
