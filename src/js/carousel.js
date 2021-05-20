9663f223cd73b64907e999bdf4fb879a7ae203c6
Demilicz <Necrolick92@gmail.com>
2021-05-20 21:00:33 +0200

    it's almost full but need a footer

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -35,3 +35,96 @@ $(function() {
     ]
   });
 });
+
+
+$(function() {
+
+  $('.events__container ').slick({
+    draggable: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
+    responsive: [
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 578,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+        }
+      }
+    ]
+  });
+});
+
+$(function() {
+  $('.countries__container').slick({
+    draggable: false,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
+    responsive: [
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 578,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: true,
+        }
+      }
+    ]
+  });
+});
+
+$(function() {
+  $('.testomonial__slider').slick({
+
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+
+    responsive: [
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          variableWidth: false,
+        }
+      },
+
+     
+    ]
+  });
+});
